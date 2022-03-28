6ce53c87bcce5acd7f097c2db8320929304b19f8
steve <steve.nguyen2013@gmail.com>
2022-03-27 17:33:35 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+function superbowlWin(record) {
+    const superbowl = record.find(element => element.result === "W")
+    if (superbowl) {
+        return superbowl.year
+    }
+    else { return undefined }
+}
+
+
+//return (superbowl ? superbowl.year : superbowl);
+
+// return superbowl ? superbowl.year : record;
+
+
+  //return superbow.year
+
+
+
+  //if (superbowl === "W") {
+   // return undefined;
+//} else {
+   // return superbowl.year;
+//}
+
+
+//return superbowl ? "2015" : undefined;
